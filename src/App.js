649640b19069227ba86f39571cc4f4671cd10c70
AntonChern888 <troubemarker123404@gmail.com>
2023-12-20 02:33:58 +0300

    check321

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Чернов Антон Игоревич 123";
+  const greeting = "Это тестовый проект Чернов Антон Игоревич 321";
 
   return (
     <div className="App">
